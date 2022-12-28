aa1b7af62ecc6bf96121afeee854c2d15088ffd4
Saurav Raj <saurav.raj5592+git@gmail.com>
2022-12-28 23:24:57 +0530

    Removed hard coded port

--- server/services/render.js
+++ server/services/render.js
@@ -1,7 +1,8 @@
 const axios = require("axios");
+const PORT=process.env.PORT
 
 exports.homeRoute = (req, res) => {
-    url = "http://localhost:8888/api/task";
+    url = `http://localhost:${PORT}/api/task`;
     axios.get(url).then((response) => {
         res.render("index", {
             title: "Home Page",
@@ -11,7 +12,7 @@ exports.homeRoute = (req, res) => {
 };
 
 exports.updateTask = (req, res) => {
-    url = `http://localhost:8888/api/task`;
+    url = `http://localhost:${PORT}/api/task`;
     axios.get(url, { params: { id: req.query.id } }).then((task) => {
         res.render("update_task", {
             title: "Update Task",
